edf48b83cfecdf7427b6fd69782964d27fb8ee65
Roy Hyunjin Han <rhh@crosscompute.com>
2017-10-24 13:02:18 -0400

    Use show_raw_output

--- test_examples.py
+++ test_examples.py
@@ -5,22 +5,22 @@ from pandas import read_csv
 def test_add_integers(tmpdir):
     args = str(tmpdir), 'add-integers', {'x_integer': 2, 'y_integer': 3}
     r = run(*args)
-    assert r['standard_output'] == '5'
+    assert r['raw_output'] == '5'
     s = serve(*args)[0]
-    assert s.find(id='standard_output-meta').text.strip() == '5'
+    assert s.find(id='raw_output-meta').text.strip() == '5'
 
 
 def test_find_primes(tmpdir):
     args = str(tmpdir), 'find-primes', {'x_integer': 2016}
     r = run(*args)
-    assert r['standard_outputs']['unique_factor_count'] == 3
+    assert r['raw_outputs']['unique_factor_count'] == 3
 
 
 def test_count_words(tmpdir):
     args = str(tmpdir), 'count-words', {'document_text_path': 'words.txt'}
     r = run(*args)
-    assert r['standard_outputs']['word_count'] == 113
-    t = read_csv(r['standard_outputs']['word_count_table_path'])
+    assert r['raw_outputs']['word_count'] == 113
+    t = read_csv(r['raw_outputs']['word_count_table_path'])
     assert t['count'].sum() == 113
     args = str(tmpdir), 'count-words', {
         'document_text_txt': 'curae canitiem inducunt'}
