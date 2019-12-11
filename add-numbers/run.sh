ffa058bbb1d5f1e29cb45165c2cee22ac39ca46c
Roy Hyunjin Han <rhh@crosscompute.com>
2019-12-11 15:20:36 -0500

    Add quotes to handle spaces and quotes properly

--- add-numbers/run.sh
+++ add-numbers/run.sh
@@ -2,5 +2,5 @@
 pip install \
     pandas==0.25.3
 python run.py \
-    $CROSSCOMPUTE_INPUT_FOLDER \
-    $CROSSCOMPUTE_OUTPUT_FOLDER
+    "$CROSSCOMPUTE_INPUT_FOLDER" \
+    "$CROSSCOMPUTE_OUTPUT_FOLDER"
