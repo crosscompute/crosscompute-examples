05af6c592f3855955e7c2ba5b7976c338ad9b881
Roy Hyunjin Han <rhh@crosscompute.com>
2020-09-05 23:15:45 +0000

    Save temporary fix

--- add-numbers/run.py
+++ add-numbers/run.py
@@ -8,6 +8,10 @@ output_folder = argv[2]
 input_path = join(input_folder, 'numbers.json')
 output_path = join(output_folder, 'sum.json')
 
+print(input_path)
+print(output_path)
+# TODO: Fix client view coercion
+
 value_by_key = json.load(open(input_path, 'rt'))
-value_by_key['c'] = value_by_key['a'] + value_by_key['b']
+value_by_key['c'] = float(value_by_key['a']) + float(value_by_key['b'])
 json.dump(value_by_key, open(output_path, 'wt'))
