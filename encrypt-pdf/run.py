d024cb157244ca8acdb7d6d43ad9083f0e38da01
Rodrigo <rmGuarachi@gmail.com>
2020-12-29 16:04:36 -0500

    adding extract pdf pages and encrypt pdf

---
+++ encrypt-pdf/run.py
@@ -0,0 +1,28 @@
+import json
+from os.path import join
+from sys import argv
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+def encrypt_pages_pdf(input_path, output_path, password):
+    pdf_writer = PdfFileWriter()
+    pdf_reader = PdfFileReader(input_path)
+
+    for page in range(pdf_reader.getNumPages()):
+        pdf_writer.addPage(pdf_reader.getPage(page))
+
+    pdf_writer.encrypt(user_pwd=password, owner_pwd=None,
+                       use_128bit=True)
+
+    with open(output_path, 'wb') as fh:
+        pdf_writer.write(fh)
+
+
+if __name__ == '__main__':
+    input_folder = argv[1]
+    output_folder = argv[2]
+    password_dictionary = json.load(open(join(input_folder, 'password.json')))
+    password = password_dictionary['password']
+    input_path = join(input_folder, pdf_file_name)
+    output_path = join(output_folder, 'output_' + pdf_file_name)
+    encrypt_pages_pdf(path, output_path, password)
