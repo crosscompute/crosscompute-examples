669b7cd9055a0592a76526a868636dd49ddd8456
Roy Hyunjin Han <rhh@crosscompute.com>
2021-08-31 15:44:58 -0400

    Update watch-cpu-history and watch-ram-history

--- widgets/watch-cpu-history/run.py
+++ widgets/watch-cpu-history/run.py
@@ -15,6 +15,9 @@ def plot(input_folder, output_folder, log_folder, debug_folder):
         csv_file.seek(0)
         csv_reader = csv.reader(csv_file)
         xs, ys = zip(*csv_reader)
+        xs = [float(_) for _ in xs]
+        ys = [float(_) for _ in ys]
 
-    plt.scatter(xs, ys)
+    ax = plt.scatter(xs, ys)
+    ax.axes.get_xaxis().set_visible(False)
     plt.savefig(join(output_folder, 'cpu.png'))
--- widgets/watch-ram-history/run.py
+++ widgets/watch-ram-history/run.py
@@ -1,9 +1,23 @@
-import json
+import csv
+import matplotlib.pyplot as plt
 import psutil
 from os.path import join
+from time import time
 
 
-def plot(input_folder, output_folder):
-    json.dump({
-        'ram-usage': psutil.virtual_memory().percent,
-    }, open(join(output_folder, 'variables.json'), 'wt'))
+def plot(input_folder, output_folder, log_folder, debug_folder):
+    unix_time = time()
+    ram_percent = psutil.virtual_memory().percent
+
+    with open(join(log_folder, 'ram-usage.csv'), 'a+t') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow([unix_time, ram_percent])
+        csv_file.seek(0)
+        csv_reader = csv.reader(csv_file)
+        xs, ys = zip(*csv_reader)
+        xs = [float(_) for _ in xs]
+        ys = [float(_) for _ in ys]
+
+    ax = plt.scatter(xs, ys)
+    ax.axes.get_xaxis().set_visible(False)
+    plt.savefig(join(output_folder, 'ram.png'))
