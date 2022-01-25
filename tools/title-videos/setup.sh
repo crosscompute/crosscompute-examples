57e4540706cab92ca8358fdb1b57e9a85525865a
Roy Hyunjin Han <rhh@crosscompute.com>
2022-01-25 15:55:08 -0500

    Move title-videos

--- tools/title-videos/setup.sh
+++
@@ -1,9 +0,0 @@
-if ! command -v ffmpeg; then
-    sudo dnf install -y \
-        https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-        https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-    sudo dnf install -y \
-        ffmpeg
-fi
-pip install -U \
-    youtube_dl
