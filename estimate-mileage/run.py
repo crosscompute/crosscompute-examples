339895c880887938374028b0f6b6e2b3e5c41bab
Roy Hyunjin Han <rhh@crosscompute.com>
2026-05-03 10:29:56 -0400

    Save notes

--- estimate-mileage/run.py
+++ estimate-mileage/run.py
@@ -5,6 +5,7 @@ from numpy import interp
 
 
 rows = [
+    (datetime(2020, 11, 15), 60646),
     (datetime(2021, 10, 16), 77107),
     (datetime(2022, 2, 12), 82176),
     (datetime(2022, 6, 9), 87267),
@@ -20,6 +21,11 @@ rows = [
     (datetime(2024, 10, 11), 121046),
     (datetime(2024, 11, 30), 123079),
     (datetime(2025, 1, 29), 126248),
+    (datetime(2025, 5, 10), 129887),
+    (datetime(2025, 9, 24), 132082),
+    (datetime(2026, 1, 12), 136215),
+    (datetime(2026, 2, 2), 137130),
+    (datetime(2026, 4, 12), 140566),
 ]
 xs = [_[0].timestamp() for _ in rows]
 ys = [_[1] for _ in rows]
@@ -33,7 +39,8 @@ datetimes = [
     datetime(2022, 12, 31),
     datetime(2023, 12, 31),
     datetime(2024, 12, 31),
-    datetime(2025, 12, 31)]
+    datetime(2025, 12, 31),
+    datetime(2026, 12, 31)]
 ts = [_.timestamp() for _ in datetimes]
 for t in ts:
     print(datetime.fromtimestamp(t), int(interp(t, xs, ys)))
